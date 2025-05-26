a7808138f0d303ffdff0598332ff3bee7754d2b2
Jakub Dąbrowski <60621737+tuniemanicku@users.noreply.github.com>
2025-05-26 14:20:02 +0200

    Finished code for generating all plots

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-import math
 
 def readFromFile(filename):
     data = pd.read_csv(filename, header=None)
@@ -9,8 +8,6 @@ def readFromFile(filename):
 
 first_x, first_y = readFromFile("2018_paths/100.csv")
 second_x, second_y = readFromFile("2018_paths/SpacerniakGdansk.csv")
-third_x, third_y = readFromFile("2018_paths/chelm.csv")
-fourth_x, fourth_y = readFromFile("2018_paths/GlebiaChallengera.csv")
 
 figure, axis = plt.subplots(2)
 axis[0].plot(first_x, first_y)
@@ -22,22 +19,11 @@ axis[1].plot(second_x, second_y)
 axis[1].set_title("Spacerniak Gdańsk")
 axis[1].set_xlabel("x [m]")
 axis[1].set_ylabel("y [m]")
-"""
-axis[0, 1].plot(third_x, third_y)
-axis[0, 1].set_title("Wielki Kanion Kolorado")
-axis[0, 1].set_xlabel("x [m]")
-axis[0, 1].set_ylabel("y [m]")
 
-axis[1, 1].plot(fourth_x, fourth_y)
-axis[1, 1].set_title("Redlujjj")
-axis[1, 1].set_xlabel("x [m]")
-axis[1, 1].set_ylabel("y [m]")
-"""
 plt.subplots_adjust(hspace=0.6)
 plt.show()
 
 #interpolation
-#interpolate the given data and create plots
 
 def normalize(arr, t_min, t_max):
     norm_arr = []
@@ -49,30 +35,22 @@ def normalize(arr, t_min, t_max):
     return norm_arr
 
 def LangrangeInterpolation(x, y, xi, N):
-    #take exactly n evenly spaced out elements from array
     idx = np.round(np.linspace(0, len(x) - 1, N)).astype(int)
     inter_x = x[idx]
     inter_y = y[idx]
     inter_x = normalize(inter_x, 0, 1)
-    n = len(inter_y)
+    length = len(inter_y)
     result = 0.0
-    for i in range(n):
-
-        # Compute individual terms of above formula
+    for i in range(length):
         term = inter_y[i]
-        for j in range(n):
+        for j in range(length):
             if j != i:
                 term = term * (xi - inter_x[j]) / (inter_x[i] - inter_x[j])
-
-        # Add current term to result
         result += term
-
     return result
 
 def plotLagrange(x,y,N=128,name="default"):
     plot_min_number = np.round(np.log2(N)-3).astype(int)
-    #figure, axis = plt.subplots(4)
-    #figure.suptitle(f"Interpolacja metodą Lagrange'a trasy {name} stosując N węzłów")
     for row in range(4):
         n = 2**(plot_min_number + row)
         domain_x = np.linspace(0.0, 1.0, len(x))
@@ -92,7 +70,6 @@ def plotLagrange(x,y,N=128,name="default"):
 
 #plotLagrange(first_x, first_y, N=128, name="100")
 #plotLagrange(second_x, second_y, N=128, name="Spacerniak Gdańsk") #<----------------------------------------
-#plotLagrange(third_x, third_y, N=128, name="Wielki Kanion")
 
 def splineInterpolation(x, y, xi, N):
     idx = np.round(np.linspace(0, len(x) - 1, N)).astype(int)
@@ -141,8 +118,6 @@ def splineInterpolation(x, y, xi, N):
 
 def plotSpline(x,y,N=128,name="default"):
     plot_min_number = np.round(np.log2(N)-3).astype(int)
-    #figure, axis = plt.subplots(4)
-    #figure.suptitle(f"Interpolacja metodą Splajnów trzeciego stopnia trasy {name} stosując N węzłów")
     for row in range(4):
         n = 2**(plot_min_number + row)
         domain_x = np.linspace(0.0, 1.0, len(x))
@@ -160,57 +135,53 @@ def plotSpline(x,y,N=128,name="default"):
         plt.subplots_adjust(hspace=0.6)
         plt.show()
 
-#plotSpline(first_x, first_y, N=128, name="100")
+#plotSpline(first_x, first_y, N=128, name="100") #<-----------------------------------------------
 #plotSpline(second_x, second_y, N=128, name="Spacerniak Gdańsk")
-"""
-def getChebyshev0to1(n):
-    nodes = np.array([ math.cos(((k-0.5)*math.pi)/n) for k in range(1,n+1) ])
-    nodes += 1
-    nodes = np.flip(normalize(nodes, 0, 1))
-    return nodes
 
-def LangrangeInterpolationChebyshev(x, y, xi, N):
-    #take exactly n evenly spaced out elements from array
-    idx = np.round(getChebyshev0to1(N)*(len(x)-1)).astype(int)
-    inter_x = x[idx]
-    inter_y = y[idx]
-    inter_x = normalize(inter_x, 0, 1)
-    n = len(inter_y)
-    result = 0.0
-    for i in range(n):
-
-        # Compute individual terms of above formula
-        term = inter_y[i]
-        for j in range(n):
+def Lagrangeinterpolation2(x, y, xi):
+    length = len(x)
+    result = 0
+    for i in range(length):
+        term = y[i]
+        for j in range(length):
             if j != i:
-                term = term * (xi - inter_x[j]) / (inter_x[i] - inter_x[j])
-
-        # Add current term to result
+                term *= (xi - x[j]) / (x[i] - x[j])
         result += term
-
     return result
 
-def plotLagrangeChebyshev(x,y,N=128,name="default"):
-    plot_min_number = np.round(np.log2(N)-3).astype(int)
-    figure, axis = plt.subplots(4)
-    figure.suptitle(f"Interpolacja metodą Lagrange'a trasy {name} stosując N węzłów")
+def generateChebyshevNodes(n, length):
+    nodes = []
+    for k in range(n):
+        x_k = np.cos((2 * k + 1) * np.pi / (2 * n))
+        index = int(round(0.5 * (x_k + 1) * (length - 1)))
+        nodes.append(index)
+    return np.array(nodes)
+
+def plotLagrangeChebyshev(x, y, name, N=16):
+    plot_min_number = np.round(np.log2(N)).astype(int)
     for row in range(4):
         n = 2**(plot_min_number + row)
-        domain_x = np.linspace(0.0, 1.0, len(x))
-        domain_y = [LangrangeInterpolation(x,y,domain_x[i], n) for i in range(len(x))]
-        domain_x = normalize(domain_x, 0.0, x[-1])
-        axis[row].plot(domain_x, domain_y)
-        axis[row].plot(x,y)
-        idx = np.round(getChebyshev0to1(n)*(len(x)-1)).astype(int)
-        axis[row].scatter(x[idx],y[idx])
-        axis[row].set_ylim(np.min(y) * 0.75, np.max(y) * 1.5)
-        axis[row].legend(["interpolowane","oryginalne","węzły"])
-        axis[row].set_title(f"N = {n}")
-        axis[row].set_xlabel("x [m]")
-        axis[row].set_ylabel("y [m]")
-    plt.subplots_adjust(hspace=0.6)
-    plt.show()
+        chebyshev_indices = generateChebyshevNodes(n, len(x))
+        chebyshev_indices = np.unique(chebyshev_indices)
 
-plotLagrangeChebyshev(first_x, first_y, N=64, name="100")
-#plotLagrangeChebyshev(second_x, second_y, N=64, name="100")
-"""
+        nodes_x = x[chebyshev_indices]
+        nodes_x /= np.max(nodes_x)
+        nodes_y = y[chebyshev_indices]
+
+        inter_x = np.linspace(0.0, 1.0, len(x))
+        inter_y = [Lagrangeinterpolation2(nodes_x, nodes_y, x) for x in inter_x]
+
+        inter_x *= np.max(x)
+        nodes_x *= np.max(x)
+
+        plt.plot(inter_x, inter_y)
+        plt.plot(x, y)
+        plt.scatter(nodes_x, nodes_y)
+        plt.ylim(np.min(y) * 0.75, np.max(y) * 1.5)
+        plt.xlabel('x [m]')
+        plt.ylabel('y [m]')
+        plt.title(f"Interpolacja metodą Lagrange'a trasy {name} stosując N = {n} węzłów Chebysheva")
+        plt.legend(["interpolowane","oryginalne","węzły"])
+        plt.show()
+
+#plotLagrangeChebyshev(second_x,second_y,"Gdansk") #<--------------------------------------------
